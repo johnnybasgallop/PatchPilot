546269138b2c81a3d34ae6201f6d2fe8084a59a3
johnnybasgallop <johnnybasgallopwork@outlook.com>
2026-02-16 17:29:56 +0000

    added in file tree + file reading functionalities

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 import json
 import os
-
+import base64
 from dotenv import load_dotenv
 from fastapi import FastAPI, Response
 from fastapi.responses import RedirectResponse
@@ -69,3 +69,26 @@ async def get_repos():
         dic[item['name']] = item
 
     return dic
+
+
+@app.get('/repo-tree')
+async def get_repo_tree(owner:str, repo:str, branch_name:str="main"):
+    headers = {
+        'Accept': 'application/vnd.github+json',
+        'Authorization': f'Bearer {access_token}'
+    }
+    
+    async with AsyncClient() as client:
+        print(owner, repo)
+        response = await client.get(f'https://api.github.com/repos/{owner}/{repo}/git/trees/main?recursive=1', headers=headers)
+    res = response.json()
+    tree = res.get("tree")
+    
+    for item in tree:
+        if item.get("path") == "requirements.txt":
+            url = item.get("url")
+            async with AsyncClient() as client:
+                response = await client.get(url=url, headers=headers)
+            res = response.json()
+            content = base64.b64decode(res.get('content')).decode('utf-8')
+            return content
